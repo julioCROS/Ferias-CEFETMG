4087dac86a130acd16a5a3dba70c3b7cf773097a
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-01-28 05:19:08 -0300

    Fixing issues

--- src/controllers/postTweet.js
+++ src/controllers/postTweet.js
@@ -3,21 +3,11 @@ const writeTweet = require("./writeTweet.js")
 const Twit = require("twit");
 
 // Creating auth bot:
-/*
 const bot = new Twit({
     consumer_key: process.env.CONSUMER_KEY,  
     consumer_secret: process.env.CONSUMER_SECRET,    
     access_token: process.env.ACCESS_TOKEN,  
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000
-});
-*/
-
-const bot = new Twit({
-    consumer_key: "Xw4F4lsVsCYisSbmV9wgNzXtj",  
-    consumer_secret: "HjnJXmRDiV9laZU0HgWLkCZ8dvW5YaI2r1lwjZe5yusAB0biAD",    
-    access_token: "1485743747400929285-85FfNQ0OFz4QnbESU4qmyz3ojJlCC4",  
-    access_token_secret: "x9UiXufChSXCafuFV8XcuayikKOSHQ8L09F1gK3KfNPH1",
     timeout_ms: 60 * 1000
 });
 
